4674c5e6e9070e8956b4c9d5d82a3a3ac1fc8d71
csjdraj <csjdraj@googlemail.com>
2026-01-18 14:54:01 +0800

    Update product images, delete obsolete items, cleanup scripts

--- scripts/debug-import.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const pdf = require('pdf-parse');
-
-const ORDER_DATA_DIR = path.join(__dirname, '../Order Data');
-
-async function debugPdf() {
-    // ...
-}
